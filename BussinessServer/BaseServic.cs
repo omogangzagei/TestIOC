e5c846170148c96d4f193fda935d7cc3804dbe71
omogangzagei <3391344773@qq.com>
2019-05-20 09:42:35 +0800

    添加项目文件。

---
+++ BussinessServer/BaseServic.cs
@@ -0,0 +1,51 @@
+﻿using BussinessInterface;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFModel;
+
+namespace BussinessServer
+{
+    public class BaseServic : IBaseService
+    {
+        protected DbContext dbContext { get; private set; }
+
+        /// <summary>
+        /// 构造函数注入
+        /// </summary>
+        /// <param name="db"></param>
+        public BaseServic(DbContext db)
+        {
+            this.dbContext = db;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 新增
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public T Insert<T>(T t) where T : class
+        {
+            dbContext.Set<T>().Add(t);
+            this.Commit();
+            return t;
+        }
+
+        /// <summary>
+        /// 提交
+        /// </summary>
+        public void Commit()
+        {
+            this.dbContext.SaveChanges();
+        }
+    }
+}
---
+++ Project/Program.cs
@@ -0,0 +1,60 @@
+﻿using EFModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Unity;
+using Microsoft.Practices.Unity.Configuration;
+using BussinessInterface;
+using BussinessServer;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 依赖了细节
+            //using (OEMREntities dbcontext=new OEMREntities())
+            //{
+            //    OEMRSysDictionry sysDictionry = new OEMRSysDictionry();
+            //    sysDictionry.DicCode = "BasicEleType";
+            //    sysDictionry.DicName = "类型";
+            //    sysDictionry.DicValue = "1";
+            //    sysDictionry.DicShowValue = "文本";
+            //    sysDictionry.DicIsValidity = "1";
+            //    dbcontext.OEMRSysDictionry.Add(sysDictionry);
+            //   OEMRTest test = new OEMRTest();
+            //    test.Name = "111";
+            //    //dbcontext.OEMRTest.Attach(test);
+            //    dbcontext.OEMRTest.Add(test);
+            //    dbcontext.SaveChanges();
+            //}
+            #endregion
+
+
+            #region IOC+EF
+            UnityConfigurationSection unity = (UnityConfigurationSection)ConfigurationManager.GetSection(UnityConfigurationSection.SectionName);
+            UnityContainer unityContainer = new UnityContainer();
+            unity.Configure(unityContainer, "MyContainer");
+
+            IBaseService sever = unityContainer.Resolve<IBaseService>();
+            //using (IBaseService sever= unityContainer.Resolve<IBaseService>())
+            //{
+            //    OEMRTest test = new OEMRTest();
+            //    test.Name = "111";
+            //    sever.Insert<OEMRTest>(test);
+            //}
+
+
+
+
+            #endregion
+
+            Console.Read();
+
+        }
+    }
+}
